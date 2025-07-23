5b56c8d831d2ce3356c493c38903670818d2204b
abelpetik <axocska@gmail.com>
2025-07-23 17:34:55 +0200

    Add trial type generator helper function

--- tutorials/helper_functions.py
+++ tutorials/helper_functions.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def detect_trial_type(behav_data, task='reversal_learning'):
     """Determine trial type from behavioral data."""
@@ -9,4 +10,15 @@ def detect_trial_type(behav_data, task='reversal_learning'):
         else:
             behav['trial_type'].append(behav['action'][i] * 4 + behav['stage2'][i] * 2 + behav['reward'][i])
 
+    return behav
+
+
+
+def detect_trial_type_ibl(behav_data):
+    """Determine trial type from behavioral data."""
+    behav = behav_data
+    behav['trial_type'] = []
+    for i in range(len(behav['Choice'])):
+        behav['trial_type'].append(behav['Choice_prev'][i] * 4 + (np.sign(behav['Reward_prev'][i]) > 0).astype(int) * 2 + (np.sign(behav['Stimulus'][i]) > 0).astype(int))
+    # TODO: this conversion doesnt care about 0 contrast stimuli it groups them with something else
     return behav
